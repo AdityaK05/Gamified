5bdfb7256dbf72df8e800058f6692a0bcf7383ee
Aditya Kaushik <145690608+AdityaK05@users.noreply.github.com>
2024-10-20 10:27:13 +0530

    Update achievement.js

--- achievement.js
+++ achievement.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const achievementSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  icon: { type: String, required: true },
+  requirement: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Achievement', achievementSchema);
